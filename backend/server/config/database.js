d10797a49b2066aebb6b0f2d911a3ab28fce7e0d
Rubén Torres <rubentorresshazam@gmail.com>
2026-09-09 19:19:08 -0300

    ✅ BD: Nombres de variables coincidiendo con Render

--- backend/server/config/database.js
+++ backend/server/config/database.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NOMBRE,
+  user: process.env.DB_USUARIO,
+  password: process.env.DB_CONTRASENA,
   ssl: {
     rejectUnauthorized: false
   },
