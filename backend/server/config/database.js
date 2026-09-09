264c4200528001d3ba1ae7294a2cad385b5f37a8
Rubén Torres <rubentorresshazam@gmail.com>
2026-09-09 19:22:36 -0300

    ✅ BD: Volviendo a URL completa

--- backend/server/config/database.js
+++ backend/server/config/database.js
@@ -1,17 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NOMBRE,
-  user: process.env.DB_USUARIO,
-  password: process.env.DB_CONTRASENA,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  },
-  max: 5,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 15000
+  }
 });
 
 module.exports = pool;
